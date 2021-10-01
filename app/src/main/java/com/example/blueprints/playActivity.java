e1d35fa2adc04c8c2556fdec808ef84b4278e419
Nonelela4980 <nonelelacele@gmail.com>
2021-10-01 06:49:54 +0200

    created the play activity UI

--- app/src/main/java/com/example/blueprints/playActivity.java
+++ app/src/main/java/com/example/blueprints/playActivity.java
@@ -16,7 +16,7 @@ import com.example.blueprints.models.Wood;
 
 public class playActivity extends AppCompatActivity  {
 
-    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -25,36 +25,6 @@ public class playActivity extends AppCompatActivity  {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide(); //hide the actionbar for this activity
         setContentView(R.layout.activity_play);
-
-
-        InstructionGive card=new InstructionGive(360,new Wood(3));
-         imageView=findViewById(R.id.imageView5);
-
-        imageView.setImageResource(card.get_drawable());
-
     }
 
-    float x,y,dx,dy;
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            x=event.getX();
-            y=event.getY();
-        }
-
-        if(event.getAction()==MotionEvent.ACTION_MOVE){
-            dx=event.getX()-x;
-            dy=event.getY()-y;
-
-            imageView.setX(imageView.getX()+dx);
-            imageView.setY(imageView.getY()+dy);
-
-            x=event.getX();
-            y=event.getY();
-        }
-
-        return super.onTouchEvent(event);
-    }
 }
