c34a86e4997065cc3f26e44ccf8fe02eb26dea33
Kanku <s219366829@mandela.ac.za>
2021-11-18 06:24:40 +0200

    onTouch listener

--- app/src/main/java/com/example/blueprints/ItemTouchListener.java
+++ app/src/main/java/com/example/blueprints/ItemTouchListener.java
@@ -5,16 +5,48 @@ import android.view.MotionEvent;
 import android.view.View;
 
 public class ItemTouchListener implements View.OnTouchListener {
+
+    private float startingPointerX;
+    private float startingPointerY;
+    private float startingViewX;
+    private float startingViewY;
+
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
-        if(motionEvent.getAction()==MotionEvent.ACTION_DOWN){
-            ClipData data=ClipData.newPlainText("","");
+//        if(motionEvent.getAction()==MotionEvent.ACTION_DOWN){
+//            ClipData data=ClipData.newPlainText("","");
+//            View.DragShadowBuilder shadowBuilder=new View.DragShadowBuilder(view);
+//            view.startDragAndDrop(data,shadowBuilder,view,0);
+//            view.setVisibility(View.INVISIBLE);
+//            return true;
+//        }
+//        else
+//            return false;
+        switch (motionEvent.getActionMasked())
+        {
+            case MotionEvent.ACTION_DOWN:
+                startingViewX=view.getX();
+                startingViewY=view.getY();
+
+                startingPointerX=motionEvent.getX();
+                startingPointerY=motionEvent.getY();
+
+                float dx=startingPointerX;
+                float dy=startingPointerY;
+
+                float viewX=startingViewX+dx;
+                float viewY=startingViewY+dy;
+
+                view.setX(viewX);
+                view.setY(viewY);
+                ClipData data=ClipData.newPlainText("","");
             View.DragShadowBuilder shadowBuilder=new View.DragShadowBuilder(view);
             view.startDragAndDrop(data,shadowBuilder,view,0);
             view.setVisibility(View.INVISIBLE);
-            return true;
+            break;
+            default:return false;
+            //case MotionEvent.ACTION_HOVER_ENTER
         }
-        else
-            return false;
+        return true;
     }
 }
